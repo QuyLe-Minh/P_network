37e49c247c60d94c0b3c23988310cf32c26519e7
Quy Le <leminhquytp@gmail.com>
2025-12-28 21:09:12 +0700

    rename folder

--- run_rt_eval.sh
+++ run_rt_eval.sh
@@ -2,6 +2,6 @@
 DATASET_PATH=DATASET_Synapse
 export PYTHONPATH=./
 export RESULTS_FOLDER=output_synapse
-export unetr_pp_preprocessed="$DATASET_PATH"/unetr_pp_raw/unetr_pp_raw_data/Task02_Synapse
-export unetr_pp_raw_data_base="$DATASET_PATH"/unetr_pp_raw
+export p_network_preprocessed="$DATASET_PATH"/unetr_pp_raw/unetr_pp_raw_data/Task02_Synapse
+export p_network_raw_data_base="$DATASET_PATH"/unetr_pp_raw
 python3 unetr_pp/rt_inference/running_scripts_rt.py unetr_pp/rt_inference/model.trt unetr_pp/rt_inference/ -p unetr_pp/rt_inference/unetr_pp_Plansv2.1_plans_3D.pkl
